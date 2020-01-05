93fa310e220e884945546a136875b3483e3ac59e
mustafidatun <mustafidatunnashihah@gmail.com>
2020-01-05 10:47:56 +0700

    update penambahan layer pada map untuk restoran

--- content/peta_utama.php
+++ content/peta_utama.php
@@ -12,7 +12,7 @@
     <script src="assets/js/Control.Scale.js"></script>
     <script src="assets/pancontrol/L.Control.Pan.js" ></script>  
     <!--<script src="http://maps.google.com/maps/api/js?v=3.2&sensor=true"></script>-->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAWR4K0m_9VINVwNbNVBn8HhcNlKvE4yhA&callback=initMap" type="text/javascript"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAWR4K0m_9VINVwNbNVBn8HhcNlKvE4yhA&callback=initMap"></script>
     <script src="assets/js/Google.js"></script>
     <script src="assets/js/Marker.Text.js"></script>
     
@@ -26,11 +26,14 @@
       
 <!--------------------------------------------  script untuk mengatur peta  ------------------------------------------------------->    
     <script type='text/javascript'> 
-      var rsLayer = new L.LayerGroup();
-      var dokterLayer = new L.LayerGroup();
+      var chineseLayer = new L.LayerGroup();
+      var padangLayer = new L.LayerGroup();
+      var fastfoodLayer = new L.LayerGroup();
+      var cafetariaLayer = new L.LayerGroup();
+      var seafoodLayer = new L.LayerGroup();
       
-      var rsico = L.icon({
-        iconUrl: 'poin/<?php echo''.$row['foto'].'' ?>',
+      var chineseico = L.icon({
+        iconUrl: 'poin/chinese1.png',
         shadowUrl: 'leaf-shadow.png',
 
         iconSize:     [20, 20], // size of the icon
@@ -41,16 +44,23 @@
       });
       
       <?php
-      $sql="SELECT * FROM rs";
+      include"koneksi/koneksi.php";
+      $dbConn = mysqli_connect($dbServer, $dbUsr, $dbPasswd) 
+                      or die("Koneksi gagal");
+                mysqli_select_db($dbConn, $dbName);
+
+      $sql="SELECT tb_restoran.* FROM tb_restoran
+            INNER JOIN tb_kategori ON tb_kategori.id_kategori = tb_restoran.id_kategori
+            WHERE tb_kategori.nama = 'Chinese'";
       $result = mysqli_query($dbConn, $sql );
       while($row = mysqli_fetch_array($result)){
-        echo" var rsMarker = new L.Marker.Text(new L.LatLng(".$row["y"].' , '.$row["x"]."), '".$row['nama_rs']."',{icon: rsico}).bindPopup('<a href=\"content/dtl_rs.php?dtl_rs=".$row["id_rs"]."\"target=\"_blank\">".$row['nama_rs']."<br><center><img width=\"80px;\" src=\"foto/rs/".$row["foto"]."\"></center>');";
-        echo ' rsLayer.addLayer(rsMarker); ';
+        echo" var chineseMarker = new L.Marker.Text(new L.LatLng(".$row["latitude"].' , '.$row["longitude"]."), '',{icon: chineseico}).bindPopup('<a href=\"content/detail_restoran.php?id_restoran=".$row["id_restoran"]."\"target=\"_blank\">".$row['nama']."<br><center><img width=\"80px;\" src=\"foto/Chinese/".$row["foto"]."\"></center>');";
+        echo ' chineseLayer.addLayer(chineseMarker); ';
       }
       ?>
       
-      var dokterico = L.icon({
-        iconUrl: 'poin/dokter.png',
+      var padangico = L.icon({
+        iconUrl: 'poin/padang1.png',
         shadowUrl: 'leaf-shadow.png',
 
         iconSize:     [20, 20], // size of the icon
@@ -61,15 +71,102 @@
       });
       
       <?php
-      $sql="SELECT * FROM dokter ";
+      include"koneksi/koneksi.php";
+      $dbConn = mysqli_connect($dbServer, $dbUsr, $dbPasswd) 
+                      or die("Koneksi gagal");
+                mysqli_select_db($dbConn, $dbName);
+
+      $sql="SELECT tb_restoran.* FROM tb_restoran
+            INNER JOIN tb_kategori ON tb_kategori.id_kategori = tb_restoran.id_kategori
+            WHERE tb_kategori.nama = 'Padang'";
       $result = mysqli_query($dbConn, $sql );
       while($row = mysqli_fetch_array($result)){
-        echo" var dokterMarker = new L.Marker.Text(new L.LatLng(".$row["y"].' , '.$row["x"]."), '".$row['nama_dokter']."',{icon: dokterico}).bindPopup('<a href=\"content/dtl_dokter.php?dtl_dokter=".$row["id_dokter"]."\"target=\"_blank\">".$row['nama_dokter']."<br><center><img width=\"80px;\" src=\"foto/dokter/".$row["foto"]."\"></center>');";
-        echo ' dokterLayer.addLayer(dokterMarker); ';
+        echo" var padangMarker = new L.Marker.Text(new L.LatLng(".$row["latitude"].' , '.$row["longitude"]."), '',{icon: padangico}).bindPopup('<a href=\"content/detail_restoran.php?id_restoran=".$row["id_restoran"]."\"target=\"_blank\">".$row['nama']."<br><center><img width=\"80px;\" src=\"foto/Padang/".$row["foto"]."\"></center>');";
+        echo ' padangLayer.addLayer(padangMarker); ';
       }
       ?>
       
+      var fastfoodico = L.icon({
+        iconUrl: 'poin/fastfood1.png',
+        shadowUrl: 'leaf-shadow.png',
+
+        iconSize:     [20, 20], // size of the icon
+        shadowSize:   [10, 14], // size of the shadow
+        iconAnchor:   [22, 64], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 12],  // the same for the shadow
+        popupAnchor:  [-3, -16] // point from which the popup should open relative to the iconAnchor
+      });
       
+      <?php
+      include"koneksi/koneksi.php";
+      $dbConn = mysqli_connect($dbServer, $dbUsr, $dbPasswd) 
+                      or die("Koneksi gagal");
+                mysqli_select_db($dbConn, $dbName);
+
+      $sql="SELECT tb_restoran.* FROM tb_restoran
+            INNER JOIN tb_kategori ON tb_kategori.id_kategori = tb_restoran.id_kategori
+            WHERE tb_kategori.nama = 'FastFood'";
+      $result = mysqli_query($dbConn, $sql );
+      while($row = mysqli_fetch_array($result)){
+        echo" var fastfoodMarker = new L.Marker.Text(new L.LatLng(".$row["latitude"].' , '.$row["longitude"]."), '',{icon: fastfoodico}).bindPopup('<a href=\"content/detail_restoran.php?id_restoran=".$row["id_restoran"]."\"target=\"_blank\">".$row['nama']."<br><center><img width=\"80px;\" src=\"foto/Fastfood/".$row["foto"]."\"></center>');";
+        echo ' fastfoodLayer.addLayer(fastfoodMarker); ';
+      }
+      ?>
+
+      var cafetariaico = L.icon({
+        iconUrl: 'poin/cafetaria1.png',
+        shadowUrl: 'leaf-shadow.png',
+
+        iconSize:     [20, 20], // size of the icon
+        shadowSize:   [10, 14], // size of the shadow
+        iconAnchor:   [22, 64], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 12],  // the same for the shadow
+        popupAnchor:  [-3, -16] // point from which the popup should open relative to the iconAnchor
+      });
+      
+      <?php
+      include"koneksi/koneksi.php";
+      $dbConn = mysqli_connect($dbServer, $dbUsr, $dbPasswd) 
+                      or die("Koneksi gagal");
+                mysqli_select_db($dbConn, $dbName);
+
+      $sql="SELECT tb_restoran.* FROM tb_restoran
+            INNER JOIN tb_kategori ON tb_kategori.id_kategori = tb_restoran.id_kategori
+            WHERE tb_kategori.nama = 'Cafetaria'";
+      $result = mysqli_query($dbConn, $sql );
+      while($row = mysqli_fetch_array($result)){
+        echo" var cafetariaMarker = new L.Marker.Text(new L.LatLng(".$row["latitude"].' , '.$row["longitude"]."), '',{icon: cafetariaico}).bindPopup('<a href=\"content/detail_restoran.php?id_restoran=".$row["id_restoran"]."\"target=\"_blank\">".$row['nama']."<br><center><img width=\"80px;\" src=\"foto/Cafetaria/".$row["foto"]."\"></center>');";
+        echo ' cafetariaLayer.addLayer(cafetariaMarker); ';
+      }
+      ?>
+
+      var seafoodico = L.icon({
+        iconUrl: 'poin/seafood1.png',
+        shadowUrl: 'leaf-shadow.png',
+
+        iconSize:     [20, 20], // size of the icon
+        shadowSize:   [10, 14], // size of the shadow
+        iconAnchor:   [22, 64], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 12],  // the same for the shadow
+        popupAnchor:  [-3, -16] // point from which the popup should open relative to the iconAnchor
+      });
+      
+      <?php
+      include"koneksi/koneksi.php";
+      $dbConn = mysqli_connect($dbServer, $dbUsr, $dbPasswd) 
+                      or die("Koneksi gagal");
+                mysqli_select_db($dbConn, $dbName);
+
+      $sql="SELECT tb_restoran.* FROM tb_restoran
+            INNER JOIN tb_kategori ON tb_kategori.id_kategori = tb_restoran.id_kategori
+            WHERE tb_kategori.nama = 'Seafood'";
+      $result = mysqli_query($dbConn, $sql );
+      while($row = mysqli_fetch_array($result)){
+        echo" var seafoodMarker = new L.Marker.Text(new L.LatLng(".$row["latitude"].' , '.$row["longitude"]."), '',{icon: seafoodico}).bindPopup('<a href=\"content/detail_restoran.php?id_restoran=".$row["id_restoran"]."\"target=\"_blank\">".$row['nama']."<br><center><img width=\"80px;\" src=\"foto/Seafood/".$row["foto"]."\"></center>');";
+        echo ' seafoodLayer.addLayer(seafoodMarker); ';
+      }
+      ?>
+
       var googleRoadmap = new L.Google('ROADMAP');
       var cloudmade = new L.TileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=EbOl8HM0UdMC6GHcIR5Y', {attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>', 
     });
@@ -81,11 +178,11 @@
       
       var map = new L.Map('map', {center: new L.LatLng(-7.981894, 112.626503),
       zoom: 13, 
-      layers: [googleRoadmap, rsLayer , dokterLayer ]});
+      layers: [googleRoadmap, chineseLayer, padangLayer, fastfoodLayer, cafetariaLayer, seafoodLayer]});
 
       map.addControl(new L.Control.Scale());
       map.addControl(new L.Control.Layers({'Cloudmade':cloudmade, 'Mapnik':mpn, 'MapQuest':qst, 'Google Roadmap':googleRoadmap, 'Google Satellite':googleSatellite, 'Google Hybrid':googleHybrid},
-      {'Rumah Sakit':rsLayer , 'Dokter':dokterLayer }));
+      {'Chinese':chineseLayer , 'Padang':padangLayer, 'Fast Food':fastfoodLayer, 'Cafetaria':cafetariaLayer, 'Sea Food':seafoodLayer}));
       
       map.on('click', onMapClick);
 
